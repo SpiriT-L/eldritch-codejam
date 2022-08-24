f76880527260a0a67cc7123faa2dfb861559d923
LeonID <lmatskevich79@gmail.com>
2022-08-25 00:27:24 +0300

    init: init start task

---
+++ eldritch-horror/src/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+// import '../scss/style.scss';
+import '../scss/main.scss';
+import '../index.html';
+// import './task';
+import './menu';
+import './pop-up';
+import './slider';
+// import '../pets.html';
+
+// import './slider'
